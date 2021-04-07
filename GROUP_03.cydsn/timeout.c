02a2c039e10854b7418b2d38382f1f95aff4a064
Oswaldo Parra <10526065@polimi.it>
2021-04-07 17:14:00 +0200

    timer routine aggiunta

--- GROUP_03.cydsn/timeout.c
+++ GROUP_03.cydsn/timeout.c
@@ -15,7 +15,7 @@
 extern uint8_t count;
 extern uint8_t timer_compare;
 extern uint8_t stop_uart;
-void CustomTimerISR(void){
+CY_ISR(CustomTimerISR){
     count++; // il timer va in overflow ogni secondo, count sono i sewcondi trascorsi da quando il timer è stato inizializzato
     if (count>=timer_compare){
         //stop_uart=1;            //  quando raggiunge il numero di secondi immessi per il timeout, alza una flag che l'uart deve usare per reinizializzarsi
